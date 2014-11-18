c0c0fa810508335b11e6efdfc8a9c70015d70118
MIZUNE Pine <pinemz@gmail.com>
2014-11-19 03:39:04 +0900

    変数フレーズを追加

--- lib/phrase/variablePhrase.ts
+++ lib/phrase/variablePhrase.ts
@@ -6,9 +6,72 @@
 import _ = require('lodash');
 
 import basicPhrase = require('./basicPhrase');
+import args = require('./args');
 
+/**
+ * 変数を含んだテキストを出力するフレーズ
+ */
 export class VariablePhrase extends basicPhrase.BasicPhrase {
-    public getText(args: { [key: string]: string }): string {
-        return null;
+    /**
+     * 変数名
+     */
+    public variableName: string = null;
+    
+    /**
+     * 接頭語
+     */
+    public prefix = '';
+
+    /**
+     * 接尾語
+     */
+    public suffix = '';
+
+    /**
+     * フレーズの初期化を行う
+     * 変数名、接頭語、接尾語は getText() 呼び出しまでに設定すれば OK
+     */
+    public constructor(
+        id: string,
+        variableName: string = null,
+        prefix = '',
+        suffix = ''
+        ) {
+        super(id);
+
+        this.variableName = variableName;
+        this.prefix = prefix;
+        this.suffix = suffix;
     }
-} 
+
+    /**
+     * フレーズを生成して返す
+     * 変数名が無効な場合、変数値は空文字として扱う
+     */
+    public getText(args: args.PhraseArguments): string {
+        var prefix = this.prefix || '';
+        var suffix = this.suffix || '';
+        var val = this.getArgumentsValue(args, this.variableName); // 変数値
+
+        return prefix + val + suffix;
+    }
+
+    /**
+     * 変数の値を取得する
+     * 変数の値が存在しない場合、空文字として扱う (運用時を考え、エラーにはしない)
+     */
+    private getArgumentsValue(args: args.PhraseArguments, variableName: string): string {
+        // 変数名が正常でない場合
+        if (!variableName) {
+            return '';
+        }
+
+        // 引数の値が正常である場合
+        if (args && _.has(args, variableName) && _.isString(args[variableName])) {
+            return args[variableName];
+        }
+
+        // 異常時は空文字
+        return '';
+    }
+}
