90856284b8ece5dc435b511ec1f71a712278f5ca
MIZUNE Pine <pinemz@gmail.com>
2014-11-19 08:16:36 +0900

    システムテストを追加

---
+++ test/system/system.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../test.d.ts" />
+
+//  システムテスト
+
+import _ = require('lodash');
+import chai = require('chai');
+
+import tg = require('../../index');
+
+var expect = chai.expect;
+
+describe('System Test', () => {
+    it('nop');
+});
