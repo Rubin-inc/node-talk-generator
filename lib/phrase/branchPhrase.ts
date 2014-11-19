ed2da42c133b7bb54f432c8a1683f9c46a18b838
Pine Mizune <pinemz@gmail.com>
2014-11-20 06:02:46 +0900

    条件フレーズを追加

--- lib/phrase/branchPhrase.ts
+++ lib/phrase/branchPhrase.ts
@@ -6,9 +6,70 @@
 import _ = require('lodash');
 
 import basicPhrase = require('./basicPhrase');
+import condition = require('./condition');
+import args = require('./args');
 
+interface ConditionItem {
+    cond: condition.Condition;
+    text: string;
+}
+
+/**
+ * 条件フレーズ
+ */
 export class BranchPhrase extends basicPhrase.BasicPhrase {
-    public getText(): string {
-        throw 'Not Implemented';
+    private conditions: ConditionItem[] = [];
+
+    public constructor(id: string) {
+        super(id);
+    }
+
+    /**
+     * 条件とテキストを追加する
+     * 条件が無い場合、常に成立しているとして扱う
+     */
+    public add(text: string, cond: condition.Condition = null): BranchPhrase {
+        this.conditions.push({
+            cond: cond,
+            text: text
+        });
+
+        return this;
+    }
+
+    /**
+     * 条件が成立した項目からランダムに選ぶ
+     */
+    public getText(args_: args.PhraseArguments): string {
+        // 条件に合う項目を抽出する
+        // 条件が無い場合は、成立しているとみなす
+        var items = _.filter(this.conditions, cond => {
+            if (cond.cond) {
+                return cond.cond.getResult(args_);
+            }
+
+            return true;
+        });
+
+        // 条件が成立した項目からランダムに選ぶ
+        var sample = _.sample(items);
+
+        // 文字列に変換
+        return sample.text || '';
+    }
+
+    /**
+     * 条件とテキストをクリアする
+     */
+    public clear(): BranchPhrase {
+        this.conditions.length = 0;
+        return this;
+    }
+
+    /**
+     * 条件とテキストのペアの個数を返す
+     */
+    public get length(): number {
+        return this.conditions.length;
     }
 } 
